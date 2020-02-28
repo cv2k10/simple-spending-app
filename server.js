a272f27c93b3cf0f5a291f19d8e7bc333edf8029
Glitch (spending-app) <none>
2020-02-28 07:47:18 +0000

    🍏👯 Add allList page to show all records

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
 const express = require("express");
 const app = express();
-const layouts = require('express-ejs-layouts')
-const mongoose = require('mongoose');
+const layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
-const userController = require('./controllers/user');
-const spendingController = require('./controllers/spending');
-
+const userController = require("./controllers/user");
+const spendingController = require("./controllers/spending");
 
 const MONGODB_CONN = `mongodb+srv://cv1:${process.env.DB_KEY}@cv1-qfdmo.mongodb.net/${process.env.DB_COLLECTION}?retryWrites=true&w=majority`;
-mongoose.connect(
-  MONGODB_CONN,
-  { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
+mongoose.connect(MONGODB_CONN, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 const db = mongoose.connection;
 
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
 app.use(layouts);
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.render('signup')
+  res.render("signup");
 });
 
-app.get('/signup', userController.signup);
-app.post('/postUser', userController.postUser);
+app.get("/signup", userController.signup);
+app.post("/postUser", userController.postUser);
 
-app.get('/spending', spendingController.spending);
-app.post('/postSpending', spendingController.postSpending);
+app.get("/spending", spendingController.spending);
+app.post("/postSpending", spendingController.postSpending);
 
+app.get("/allList", spendingController.allList);
 
 // listen for requests :)
-db.once('open', ()=> {
-  console.log('Successfully connected to MongoDB using Mongoose!');
-  app.listen(app.get('port'), () => {
-    console.log("Your app is listening on port " + app.get('port'));
+db.once("open", () => {
+  console.log("Successfully connected to MongoDB using Mongoose!");
+  app.listen(app.get("port"), () => {
+    console.log("Your app is listening on port " + app.get("port"));
   });
 });
-
